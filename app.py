d5960a34bbb0aa15390e53c43369e76ec0a7874c
40582229 <40582229@live.napier.ac.uk>
2024-09-25 13:38:37 +0100

    api requests remember to pip install requests

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from flask import Flask
-
+from requests import get
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello World this is home"
+def  home():
+    response = get('http://127.0.0.1:5000/users').content
+    return response
 
 @app.route("/test")
 def test():
     return "This is test"
 
-@app.route("/Commit1")
-def test():
-    return "This is commit 1"
+
